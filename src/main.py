3015609e349c07f0f9abbc456a6918739b36e320
alexscoelho <alexsoncoelho21@hotmail.com>
2021-02-05 22:59:22 +0000

    first commit

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
+from flask_socketio import SocketIO, send
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
@@ -20,6 +21,9 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
+#initialize Flask-SocketIO
+socketio = SocketIO(app, cors_allowed_origins="*")
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -30,16 +34,78 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+#Create user
+@app.route('/user', methods=['POST'])
+def create_user():
 
+    body = request.get_json()
+    if body is None:
+        raise APIException("You need to specify the request body as a json object", status_code=400)
+    if 'first_name' not in body:
+        raise APIException('You need to specify the first_name', status_code=400)
+    if 'email' not in body:
+        raise APIException('You need to specify the email', status_code=400)
+    if 'password' not in body:
+        raise APIException('You need to specify the password', status_code=400)
+
+    user1 = User(first_name=body['first_name'], email=body['email'], password=body['password'])
+    db.session.add(user1)
+    db.session.commit()
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "User succesfully created"
     }
-
     return jsonify(response_body), 200
+
+#Get all users
+@app.route('/users', methods=['GET'])
+def get_users():
+
+    # get all the people
+    users_query = User.query.all()
+    all_users = list(map(lambda x: x.serialize(), users_query))
+    return jsonify(all_users), 200
+
+
+# Get or edit a user
+@app.route('/user/<int:user_id>', methods=['PUT', 'GET'])
+def get_single_user(user_id):
+    """
+    Single person
+    """
+    body = request.get_json() 
+    if request.method == 'PUT':
+        user1 = User.query.get(user_id)
+        user1.email = body["email"]
+        user1.first_name = body["first_name"]
+        db.session.commit()
+        return jsonify(user1.serialize()), 200
+
+    if request.method == 'GET':
+        user1 = User.query.get(user_id)
+        return jsonify(user1.serialize()), 200
+
+    return "Invalid Method", 404
+
+# Delete user
+@app.route('/person/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    
+    user1 = User.query.get(user_id)
+    if user1 is None:
+        raise APIException('User not found', status_code=404)
+    db.session.delete(user1)
+    db.session.commit()
+    return jsonify(user1.serialize()), 200
+
+#Event
+@socketio.on('message')
+def message(message):
+    print(message)
+    send(message, broadcast=True)
+    return None
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+    socketio.run(app, debug=True)
+    # app.run(host='0.0.0.0', port=PORT, debug=False)
